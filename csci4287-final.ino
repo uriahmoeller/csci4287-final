68c56196e7d12478796d56d0d18f0e941fc31513
uriahmoeller <uriah.moeller@gmail.com>
2019-04-15 16:56:54 -0600

    Initial header

--- csci4287-final.ino
+++ csci4287-final.ino
@@ -1,3 +1,7 @@
+//Uriah Moeller, Dalton Burke
+//Final Project - Traffic Light
+//Simulation of a one-way intersection with a one-way pedestrian crossing
+
 void setup() {
   // put your setup code here, to run once:
 

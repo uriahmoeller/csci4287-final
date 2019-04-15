3595382cca7f1ab7f71ed5d38f49a535df62d486
Dalton Burke <dmtburke@gmail.com>
2019-04-15 17:14:02 -0600

    added file description to header

--- csci4287-final.ino
+++ csci4287-final.ino
@@ -2,6 +2,9 @@
 //Final Project - Traffic Light
 //Simulation of a one-way intersection with a one-way pedestrian crossing
 
+//csci4287-final.ino
+//main project file, containing setup and loop functions
+
 void setup() {
   // put your setup code here, to run once:
 

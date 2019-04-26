1205ad9737fe8b3cbe92a2dcd918315d8986e142
uriahmoeller <uriah.moeller@gmail.com>
2019-04-26 01:35:23 -0600

    Update csci4287-final.ino
    All-in-one update for project. Should work out-of-box pending analog interrupt (ultrasonic sensor) behavior and assuming no logic errors.

--- csci4287-final.ino
+++ csci4287-final.ino
@@ -5,12 +5,223 @@
 //csci4287-final.ino
 //main project file, containing setup and loop functions
 
-void setup() {
-  // put your setup code here, to run once:
+#include "avr/io.h"
+#include "avr/interrupt.h"
 
+//Timer
+long cycleStartTime = 0;
+
+//Constants
+const long fullCycleTime = 30000;
+const long halfCycleTime = 15000;
+
+//North-bound pins
+const int northCarSensorTriggerPin = 4;
+const int northCarSensorEchoPin = 2;
+
+const int northGreenLightPin = 6;
+const int northYellowLightPin = 7;
+const int northRedLightPin = 8;
+
+const int northPedestrianButtonPin = 9;
+
+const int northPedestrianLightPin = 10;
+
+//South-bound pins
+const int southCarSensorTriggerPin = 5;
+const int southCarSensorEchoPin = 3;
+
+const int southGreenLightPin = 11;
+const int southYellowLightPin = 12;
+const int southRedLightPin = 13;
+
+const int southPedestrianButtonPin = 0;
+
+const int southPedestrianLightPin = 1;
+
+//North-bound flags
+bool northCarSensorFlag = false;
+bool northPedestrianButtonFlag = false;
+
+//South-bound flags
+bool southCarSensorFlag = false;
+bool southPedestrianButtonFlag = false;
+
+//Global states
+bool switchingDirections = false;
+enum cross_direction { NORTH, SOUTH };
+cross_direction crossingDirection = NORTH;
+
+//Light states
+enum light_color { GREEN, YELLOW, RED };
+
+//Timer compare interrupt
+ISR(TIMER1_COMPA_vect) {
+  if (!switchingDirections) {
+    if (crossingDirection == NORTH && northPedestrianButtonFlag == true) {
+      digitalWrite(northPedestrianLightPin, !digitalRead(northPedestrianLightPin));
+    } else if (crossingDirection == SOUTH && southPedestrianButtonFlag == true) {
+      digitalWrite(southPedestrianLightPin, !digitalRead(southPedestrianLightPin));
+    }    
+  } else {
+    digitalWrite(northPedestrianLightPin, LOW);
+    digitalWrite(southPedestrianLightPin, LOW);
+  }
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+//North-bound interrupt functions
+void northCarSensorISR() {
+  northCarSensorFlag = true;
+}
 
+void northPedestrianButtonISR() {
+  northPedestrianButtonFlag = true;
+}
+
+//South-bound interrupt functions
+void southCarSensorISR() {
+  southCarSensorFlag = true;
+}
+
+void southPedestrianButtonISR() {
+  southPedestrianButtonFlag = true;
+}
+
+//Direction switching sequence
+void switchDirections() {
+  switchingDirections = true;
+  if (crossingDirection == NORTH) {
+    northPedestrianButtonFlag = false;
+    
+    setActiveLight(NORTH, YELLOW);
+    delay(2000);
+    setActiveLight(NORTH, RED);
+    delay(5000);
+    setActiveLight(SOUTH, GREEN);
+    
+    crossingDirection = SOUTH;
+    southCarSensorFlag = false;
+  } else if (crossingDirection == SOUTH) {
+    southPedestrianButtonFlag = false;
+    
+    setActiveLight(SOUTH, YELLOW);
+    delay(2000);
+    setActiveLight(SOUTH, RED);
+    delay(5000);
+    setActiveLight(NORTH, GREEN);
+    
+    crossingDirection = NORTH;
+    northCarSensorFlag = false;
+  }
+  switchingDirections = false;
+  cycleStartTime = millis();
+}
+
+//Light LED color setter
+void setActiveLight (cross_direction targetDirection, light_color targetColor) {
+  if (targetDirection == NORTH) {
+    switch (targetColor) {
+      case GREEN:
+        digitalWrite(northGreenLightPin, HIGH);
+        digitalWrite(northYellowLightPin, LOW);
+        digitalWrite(northRedLightPin, LOW);
+        break;
+      case YELLOW:
+        digitalWrite(northGreenLightPin, LOW);
+        digitalWrite(northYellowLightPin, HIGH);
+        digitalWrite(northRedLightPin, LOW);
+        break;
+      case RED:
+        digitalWrite(northGreenLightPin, LOW);
+        digitalWrite(northYellowLightPin, LOW);
+        digitalWrite(northRedLightPin, HIGH);
+        break;
+    }
+  } else if (targetDirection == SOUTH) {
+    switch (targetColor) {
+      case GREEN:
+        digitalWrite(southGreenLightPin, HIGH);
+        digitalWrite(southYellowLightPin, LOW);
+        digitalWrite(southRedLightPin, LOW);
+        break;
+      case YELLOW:
+        digitalWrite(southGreenLightPin, LOW);
+        digitalWrite(southYellowLightPin, HIGH);
+        digitalWrite(southRedLightPin, LOW);
+        break;
+      case RED:
+        digitalWrite(southGreenLightPin, LOW);
+        digitalWrite(southYellowLightPin, LOW);
+        digitalWrite(southRedLightPin, HIGH);
+        break;
+    }
+  }
+}
+
+//Setup
+void setup() {
+  //1 sec timer
+  noInterrupts();
+  TCCR1A = 0; 
+  TCCR1B = 0;
+  
+  OCR1A = 15625;
+  
+  TCCR1B = (1 << WGM12);
+  TCCR1B |= ((1 << CS12) | (1 << CS10));
+  
+  TCNT1 = 0;
+  
+  TIMSK1 = (1 << OCIE1A);
+  interrupts();
+
+  //Initialize start time
+  cycleStartTime = millis();
+
+  //LED pin modes
+  pinMode(northGreenLightPin, OUTPUT);
+  pinMode(northYellowLightPin, OUTPUT);
+  pinMode(northRedLightPin, OUTPUT);
+
+  pinMode(northPedestrianLightPin, OUTPUT);
+  
+  pinMode(southGreenLightPin, OUTPUT);
+  pinMode(southYellowLightPin, OUTPUT);
+  pinMode(southRedLightPin, OUTPUT);
+  
+  pinMode(southPedestrianLightPin, OUTPUT);
+
+  //Ultrasonic sensor pin modes
+  pinMode(northCarSensorTriggerPin, OUTPUT);
+  pinMode(northCarSensorEchoPin, INPUT);
+  
+  pinMode(southCarSensorTriggerPin, OUTPUT);
+  pinMode(southCarSensorEchoPin, INPUT);
+  
+  //Attach car sensors to input interrupts
+  attachInterrupt(0, northCarSensorISR, CHANGE);
+  attachInterrupt(1, southCarSensorISR, CHANGE); //TODO: Add HIGH/LOW trigger to determine distance, or maybe just use CHANGE and ignore threshold (hope it just works, if it works remove trigger pins
+
+  //Attach pedestrian crossing buttons to input interrupts
+  attachInterrupt(digitalPinToInterrupt(northPedestrianButtonPin), northPedestrianButtonISR, CHANGE); 
+  attachInterrupt(digitalPinToInterrupt(southPedestrianButtonPin), southPedestrianButtonISR, CHANGE); 
+
+  //Initial state
+  setActiveLight(NORTH, GREEN);
+  setActiveLight(SOUTH, RED);
+}
+
+//Loop
+void loop() {
+  if (!switchingDirections) {
+    if (crossingDirection == NORTH) {
+      if (((millis() >= (cycleStartTime + halfCycleTime)) && (southCarSensorFlag || southPedestrianButtonFlag)) || (millis() >= (cycleStartTime + fullCycleTime))) {
+        switchDirections();
+      }
+    } else if (crossingDirection == SOUTH) {
+      if (((millis() >= (cycleStartTime + halfCycleTime)) && (northCarSensorFlag || northPedestrianButtonFlag)) || (millis() >= (cycleStartTime + fullCycleTime))) {
+        switchDirections();
+      }
+    }
+  }
 }
